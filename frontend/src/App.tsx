145026c3509000611200544effb1568774fc30b3
Hugh Gramelspacher <hughgramelspacher@gmail.com>
2025-03-21 19:53:59 -0700

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Hugh's portfolio. Created entirely by Hugh.</h1>
+      <h2>Project 1: Victoria 2 Save Game analyzer </h2>
+      <p>This project was made by me to analyze vic 2 save games.</p>
+      <h2>Project 2: Focuswood</h2>
+      <p>This is an ongoing project that was made by me in react. It aims to make me productive myself actually haha</p>
+    </div>
+  )
+}
+
+export default App
